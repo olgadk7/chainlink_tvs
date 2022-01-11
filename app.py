c97a6c7587ed0d3e4c133141a76a765e29a77c83
Olga Kahn <ozziekahn@gmail.com>
2022-01-10 20:52:01 -0500

    testing fetching

--- app.py
+++ app.py
@@ -15,46 +15,46 @@ from jupyter_dash import JupyterDash
 from dash import html
 from dash import dcc
 
-# # DATA IMPORT
-#
-# # read in the users to use (and users not to use)
-#
-# sheet = "1ayLr5tEUzqjdSkR8n9Q_ylirk-QcXBf87xqmaNAcwYg"
-# sources_sheet = "Sources"
-#
-# users_col="List of Chainlink users"
-# nonusers_col="List of Users to exclude form the report/analytics"
-#
-# users_list = get_names_list(sheet_id=sheet, col_name_string=users_col, sheet_name=sources_sheet)
-# nonusers_list = get_names_list(sheet_id=sheet, col_name_string=nonusers_col, sheet_name=sources_sheet)
-#
-# # print("Users list ({}".format(len(users_list)), "items): ", users_list, "\n"*2,
-# #       "Non-users list ({}".format(len(nonusers_list)), "items): ", nonusers_list)
-#
-# # remove duplicates in the non-users list
-# nonusers_list = list(set(nonusers_list))
-#
-# # market is comprised of all chainlink users and non-users
-# market = users_list + nonusers_list
-#
-# # THIS SHOULDN'T BE HARDCODED. WRITE A FUNCTION TO AUTOMATE
-# # remove duplicates
-# nonusers_list.remove('bunny')
-# users_list.remove('armor')
-#
-# # define market
-# market = users_list + nonusers_list
-#
-# # pull TVL via DeFi Llama
-#
-# # use custom function to connect to DeFi Llama API
-# users_json = get_responses(users_list)
-#
-# # get non-users (in order to get the market later by adding to 'users')
-# nonusers_json = get_responses(nonusers_list)
-#
-# # non-users, together with users make up the market
-# market_json = users_json + nonusers_json
+# DATA IMPORT
+
+# read in the users to use (and users not to use)
+
+sheet = "1ayLr5tEUzqjdSkR8n9Q_ylirk-QcXBf87xqmaNAcwYg"
+sources_sheet = "Sources"
+
+users_col="List of Chainlink users"
+nonusers_col="List of Users to exclude form the report/analytics"
+
+users_list = get_names_list(sheet_id=sheet, col_name_string=users_col, sheet_name=sources_sheet)
+nonusers_list = get_names_list(sheet_id=sheet, col_name_string=nonusers_col, sheet_name=sources_sheet)
+
+# print("Users list ({}".format(len(users_list)), "items): ", users_list, "\n"*2,
+#       "Non-users list ({}".format(len(nonusers_list)), "items): ", nonusers_list)
+
+# remove duplicates in the non-users list
+nonusers_list = list(set(nonusers_list))
+
+# market is comprised of all chainlink users and non-users
+market = users_list + nonusers_list
+
+# THIS SHOULDN'T BE HARDCODED. WRITE A FUNCTION TO AUTOMATE
+# remove duplicates
+nonusers_list.remove('bunny')
+users_list.remove('armor')
+
+# define market
+market = users_list + nonusers_list
+
+# pull TVL via DeFi Llama
+
+# use custom function to connect to DeFi Llama API
+users_json = get_responses(users_list)
+
+# get non-users (in order to get the market later by adding to 'users')
+nonusers_json = get_responses(nonusers_list)
+
+# non-users, together with users make up the market
+market_json = users_json + nonusers_json
 
 
 
@@ -65,15 +65,15 @@ from dash import dcc
 
 # just open the already pulled data
 
-# with open('users_json.pkl', 'wb') as f:
-#     pickle.dump(users_json, f)
-#     pickle.dump(nonusers_json, f)
-#     pickle.dump(market_json, f)
+with open('users_json.pkl', 'wb') as f:
+    pickle.dump(users_json, f)
+    pickle.dump(nonusers_json, f)
+    pickle.dump(market_json, f)
 
-with open('users_json.pkl', 'rb') as f:
-    users_json = pickle.load(f)
-    nonusers_json = pickle.load(f)
-    market_json = pickle.load(f)
+# with open('users_json.pkl', 'rb') as f:
+#     users_json = pickle.load(f)
+#     nonusers_json = pickle.load(f)
+#     market_json = pickle.load(f)
 
 
 
@@ -128,11 +128,11 @@ app.layout = dbc.Container([
         style={"margin-bottom": "30px"})]),
 
 dbc.Row([dcc.Markdown('''
-    Why might we want to look at TVL? Popularized by the DeFi Pulse and the Concourse Open Community that developed other DeFi public services, TVL used to represent protocol growth and / or value. It gained traction as it captured something most smart contracts and networks in a very intricate system had in common: collateral. 
+    Why might we want to look at TVL? Popularized by the DeFi Pulse and the (Concourse Open Community)[https://concourseopen.com/] that developed other DeFi public services as well, TVL used to represent protocol growth and / or value. It gained traction as it captured something most smart contracts and networks in a very intricate system had in common: collateral. 
 
-    As yield farming and liquidity mining took off, thanks to opportunities in incentive design, we have to take into account some nuances of TVL calculations if we were to make comparisons using it. For example, a big one is double counting, see [this twitter thread](https://twitter.com/sikiriki12/status/1295089928901140481?s=20) for more info. The other ones are the direction of locking for multichain token migration, and, similarly, “sovereignty” over a native asset of one chain locked on another chain (more no these [here](https://github.com/DefiLlama/DefiLlama-Adapters/pull/60#issuecomment-807045050)
+    As yield farming and liquidity mining took off, thanks to opportunities in incentive design, we have to take into account some nuances of TVL calculations if we were to make comparisons using it. For example, a big one is double counting, see [this](https://twitter.com/sikiriki12/status/1295089928901140481?s=20) twitter thread for more info. The other ones are the direction of locking for multichain token migration, and, similarly, “sovereignty” over a native asset of one chain locked on another chain (more on these [here](https://github.com/DefiLlama/DefiLlama-Adapters/pull/60#issuecomment-807045050)).
 
-    DeFi Llama, that is used here, considers any asset that is held in one of the protocol's contracts can be considered as part of TVL, with two exceptions [source](https://docs.llama.fi/list-your-project/what-to-include-as-tvl):
+    DeFi Llama, that is used here, [considers](https://docs.llama.fi/list-your-project/what-to-include-as-tvl) any asset that is held in one of the protocol's contracts can be considered as part of TVL, with two exceptions:
     * Assets on pool2, that is, money that is providing liquidity to an AMM pool where one of the tokens is from the protocol (except on some cases where those assets are performing an active function such as being used as collateral).
     * Non-crypto assets which are external to the blockchain, such as bonds or fiat currency.  
     ''', style={"margin-bottom": "50px"})]),
